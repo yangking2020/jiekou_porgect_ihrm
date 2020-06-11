7b14d75cbf8d01ebd1865af94c9a64021526e6ce
yangking2020 <1989209830@qq.com>
2020-06-11 10:22:11 +0800

    修改报告文件名

--- run_suite.py
+++ run_suite.py
@@ -7,7 +7,8 @@ from app import BASE_DIR
 # 生成测试套件
 suite = unittest.TestLoader().discover('./script','test*.py')
 # 定义报告名
-file_name = BASE_DIR +'/report/ihrm%s.html' % time.strftime('%Y%m%d%H%M%S')
+# file_name = BASE_DIR +'/report/ihrm%s.html' % time.strftime('%Y%m%d%H%M%S')
+file_name = BASE_DIR +'/report/report.html'
 # 运行并写入
 with open(file_name,'wb') as f:
     runner = HTMLTestRunner(f,title='ihrm测试报告',description='完美的报告')
